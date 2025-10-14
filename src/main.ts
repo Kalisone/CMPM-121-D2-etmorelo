37498861f9139abba5f4ba9649de753ae3ae7b81
Kalisone <emorelos04@gmail.com>
2025-10-14 07:24:40 +0000

    added clear button (STEP 2 DONE)

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,21 @@ import "./style.css";
 
 document.body.innerHTML = `
   <h1>CMPM 121 D2</h1>
+  <hr><br>
   <canvas id="canvasMain" width="256" height="256"></canvas>
+  <br><br>
+  <button id="buttonClear">Clear Canvas</button>
 `;
 
 const canvas = document.getElementById("canvasMain") as HTMLCanvasElement;
-const canvasContext = canvas.getContext("2d")!;
+const context = canvas.getContext("2d")!;
+const clear = document.getElementById("buttonClear")!;
 
 let isDrawing: boolean = false;
 let x = 0;
 let y = 0;
 
+// Draw in canvas
 canvas.addEventListener("mousedown", (e) => {
   x = e.offsetX;
   y = e.offsetY;
@@ -20,7 +25,7 @@ canvas.addEventListener("mousedown", (e) => {
 
 canvas.addEventListener("mousemove", (e) => {
   if (isDrawing) {
-    drawLine(canvasContext, x, y, e.offsetX, e.offsetY);
+    drawLine(context, x, y, e.offsetX, e.offsetY);
     x = e.offsetX;
     y = e.offsetY;
   }
@@ -28,12 +33,16 @@ canvas.addEventListener("mousemove", (e) => {
 
 canvas.addEventListener("mouseup", (e) => {
   if (isDrawing) {
-    drawLine(canvasContext, x, y, e.offsetX, e.offsetY);
+    drawLine(context, x, y, e.offsetX, e.offsetY);
     x = 0;
     y = 0;
   }
 
   isDrawing = false;
+});
+
+clear.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
 });
 
 function drawLine(
